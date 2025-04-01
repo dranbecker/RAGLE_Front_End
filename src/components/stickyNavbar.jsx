5d26878b0552215c4cfdea38b6bca60bb5c83534
Andreas <noreply@myfuji.de>
2025-04-01 02:33:20 +0200

    Einschreibe-Button positioniert -> muss aber noch mal

--- src/components/stickyNavbar.jsx
+++ src/components/stickyNavbar.jsx
@@ -32,27 +32,6 @@ const StickyNavigation = () => {
           >
             <img src={logoImg} className='h-8' alt='RAGLE Logo' />
           </Link>
-          <button
-            onClick={() => setShowModal(true)}
-            type='button'
-            className='flex items-center gap-2 py-2 px-4 text-sm font-medium text-gray-300 bg-gray-700 border border-gray-500 rounded-lg hover:bg-gray-600 hover:text-blue-400 transition-colors duration-200'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='w-5 h-5'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25H4.5a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15A2.25 2.25 0 002.25 6.75m19.5 0v.243a2.25 2.25 0 01-.659 1.591l-7.5 7.5a1.5 1.5 0 01-2.122 0l-7.5-7.5a2.25 2.25 0 01-.659-1.591V6.75'
-              />
-            </svg>
-            Einschreiben für ScienceUpdate
-          </button>
           <div className='flex items-center space-x-6 rtl:space-x-reverse'>
             <Link
               to='/login'
@@ -66,69 +45,90 @@ const StickyNavigation = () => {
 
       {/* Untere Navigationsleiste */}
       <nav className='bg-gray-700 transition-colors duration-300 border-none'>
-        <div className='max-w-screen-xl px-4 py-3 mx-auto'>
-          <div className='flex items-center'>
-            <ul className='flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm'>
-              <li>
-                <Link
-                  to='/'
-                  className={`${
-                    location.pathname === '/'
-                      ? 'text-blue-400 underline'
-                      : 'text-white'
-                  } hover:underline`}
-                  aria-current={location.pathname === '/' ? 'page' : undefined}
+        <div className='max-w-screen-xl px-4 py-4 mx-auto flex justify-center'>
+          <ul className='flex flex-row items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm'>
+            <li>
+              <Link
+                to='/'
+                className={`${
+                  location.pathname === '/'
+                    ? 'text-blue-400 underline'
+                    : 'text-white'
+                } hover:underline`}
+                aria-current={location.pathname === '/' ? 'page' : undefined}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/top-themen'
+                className={`${
+                  location.pathname === '/top-themen'
+                    ? 'text-blue-400 underline'
+                    : 'text-white'
+                } hover:underline`}
+                aria-current={
+                  location.pathname === '/top-themen' ? 'page' : undefined
+                }
+              >
+                Top-Themen
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/ragle-prime'
+                className={`${
+                  location.pathname === '/ragle-prime'
+                    ? 'text-blue-400 underline'
+                    : 'text-white'
+                } hover:underline`}
+                aria-current={
+                  location.pathname === '/ragle-prime' ? 'page' : undefined
+                }
+              >
+                RAGLE Prime
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/ueber-uns'
+                className={`${
+                  location.pathname === '/uber-uns'
+                    ? 'text-blue-400 underline'
+                    : 'text-white'
+                } hover:underline`}
+                aria-current={
+                  location.pathname === '/ueber-uns' ? 'page' : undefined
+                }
+              >
+                Über uns
+              </Link>
+            </li>
+            <li>
+              <button
+                onClick={() => setShowModal(true)}
+                type='button'
+                className='flex items-center gap-2 py-2 px-4 text-sm font-medium text-gray-300 bg-gray-700 border border-gray-500 rounded-lg hover:bg-gray-600 hover:text-blue-400 transition-colors duration-200'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-5 h-5'
                 >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to='/top-themen'
-                  className={`${
-                    location.pathname === '/top-themen'
-                      ? 'text-blue-400 underline'
-                      : 'text-white'
-                  } hover:underline`}
-                  aria-current={
-                    location.pathname === '/top-themen' ? 'page' : undefined
-                  }
-                >
-                  Top-Themen
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to='/ragle-prime'
-                  className={`${
-                    location.pathname === '/ragle-prime'
-                      ? 'text-blue-400 underline'
-                      : 'text-white'
-                  } hover:underline`}
-                  aria-current={
-                    location.pathname === '/ragle-prime' ? 'page' : undefined
-                  }
-                >
-                  RAGLE Prime
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to='/ueber-uns'
-                  className={`${
-                    location.pathname === '/uber-uns'
-                      ? 'text-blue-400 underline'
-                      : 'text-white'
-                  } hover:underline`}
-                  aria-current={
-                    location.pathname === '/ueber-uns' ? 'page' : undefined
-                  }
-                >
-                  Über uns
-                </Link>
-              </li>
-            </ul>
-          </div>
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25H4.5a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15A2.25 2.25 0 002.25 6.75m19.5 0v.243a2.25 2.25 0 01-.659 1.591l-7.5 7.5a1.5 1.5 0 01-2.122 0l-7.5-7.5a2.25 2.25 0 01-.659-1.591V6.75'
+                  />
+                </svg>
+                Einschreiben für ScienceUpdate
+              </button>
+            </li>
+          </ul>
         </div>
       </nav>
       {showModal && (
